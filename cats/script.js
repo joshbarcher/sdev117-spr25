ae6e530fb0ed52b3f304ce8a7f9837cb24817fc5
Josh Archer <jarcher@greenriver.edu>
2025-06-04 13:40:49 -0700

    Added breed data.

--- cats/script.js
+++ cats/script.js
@@ -2,10 +2,45 @@
 window.onload = () => {
     //register clicking the button
     const button = document.querySelector("#cat-btn");
-    button.onclick = clickHandler;
+    button.onclick = loadImage;
+
+    //onload we show an initial image
+    loadImage();
+    loadBreeds();
 }
 
-function clickHandler() {
+//async turns this function into a "promise" as well!
+async function loadBreeds() {
+    const url = "https://api.thecatapi.com/v1/breeds";
+    const config = {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "cab8736c-d9ae-481e-8e20-4fe581e5cb09"
+        }
+    }
+
+    //wait until the promise of fetch() is complete
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    displayBreeds(data);
+}
+
+function displayBreeds(data) {
+    const breedsSection = document.querySelector("#breeds");
+
+    for (const breed of data) {
+        const html = `<div class="breed">
+            <h2>${breed.name}</h2>
+            <p>${breed.description}</p>
+        </div>`
+
+        breedsSection.innerHTML += html;
+    }
+}
+
+function loadImage() {
     //where is the data?
     const url = "https://api.thecatapi.com/v1/images/search";
     const config = {
@@ -19,14 +54,10 @@ function clickHandler() {
     //get's the data back using a JS 'promise'
     fetch(url, config)
         .then(function(response) {
-            console.log(response);
-
             //converts JSON text to JS object
             return response.json();
         })
         .then(function(json) {
-            console.log(json);
-
             const cat = json[0];
             drawCat(cat)
         })
